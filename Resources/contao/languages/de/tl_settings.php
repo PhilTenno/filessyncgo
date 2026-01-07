974b6155644b1ba1e104f1d75593107ebd265c83
Phil <git@hardbitrocker.de>
2026-01-07 13:50:23 +0100

    fix de->languages

--- Resources/contao/languages/de/tl_settings.php
+++ Resources/contao/languages/de/tl_settings.php
@@ -1,4 +1,4 @@
 <?php
-
+//FilesSyncGo ->Resources/contao/languages/de/tl_settings.php
 $GLOBALS['TL_LANG']['tl_settings']['filessyncgo_token'] = ['meinToken', 'https://meineDomain.com/filessyncgo?token=meinToken zur Dateisynchronisation. Erlaubte Zeichen: Buchstaben, Ziffern, "-", "_", "@"'];
 $GLOBALS['TL_LANG']['tl_settings']['filessyncgo_token_invalid'] = 'Der Token darf nur Buchstaben, Ziffern sowie "-", "_" und "@" enthalten.';
